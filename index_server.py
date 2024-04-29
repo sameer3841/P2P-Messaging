61a25bcd4deb214f5c79127d0ecc469340bd9dac
Danielson Adjocy <dadjocy21@Danielsons-MacBook-Pro.local>
2024-04-29 11:35:15 -0400

    Server started

--- index_server.py
+++ index_server.py
@@ -1,3 +1,27 @@
 import socket
 import os
-import threading
+import sys
+import threading as thr
+
+IP = '127.0.0.1'
+PORT = 12000
+BUFFER_SIZE = 1024
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((IP, PORT))
+server_socket.listen(5)
+print(f'Server ready and listening on {IP}:{PORT}')
+clients = [] #List of clients, members should be tuples of IP and Ports
+try:
+    while True:  # section 4 step 6
+        (conn_socket, addr) = server_socket.accept()  # section 4 step 3
+        clients.append((conn_socket, addr))
+        #threader = thr.Thread(target=service_client_connection, args=(conn_socket,))
+        #threader.start()
+except KeyboardInterrupt as ki:
+    pass
+finally:
+    server_socket.close()
+def receive():
+    pass
+def send():# Probably gonna need multiple send and receive for each type
+    pass
