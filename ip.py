ed5b55963fb66c391921f6cdbad6b76e30439b21
Danielson Adjocy <dadjocy21@Danielsons-MacBook-Pro.local>
2024-04-30 14:09:02 -0400

    sending messages

--- ip.py
+++ ip.py
@@ -6,5 +6,6 @@ import threading as thr
 
 def get_ip_address(s: socket.socket):
     #s.connect(("8.8.8.8", 80))
-    s.connect(("10.33.16.1", 1200))
-    return s.getsockname()
+    server_address = ("10.33.16.1", 1200)
+    s.connect(server_address)
+    return s.getsockname() , server_address
--- peer.py
+++ peer.py
@@ -4,9 +4,20 @@ import sys
 import threading as thr
 import ip
 
-server_address = ("10.33.16.1", 1200)
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #s.connect(("8.8.8.8", 80))
+    server_address = ("127.0.0.1", 12000)
+    s.connect(server_address)
+    return s.getsockname() , server_address
+
 BUFFER_SIZE = 1024
 peer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-IP, PORT = ip.get_ip_address(peer)
-print(IP, PORT)
+
+address, server_address = get_ip_address()
+peer.bind((address))
+print(address)
+print(server_address)
 peer.sendto("Member 1".encode(), server_address)
+print("sent")
