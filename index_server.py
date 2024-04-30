9fc3414dff7c9b64aee80a73b263a5ad2acdc19a
Danielson Adjocy <dadjocy21@Danielsons-MacBook-Pro.local>
2024-04-30 12:41:10 -0400

    binding works

--- index_server.py
+++ index_server.py
@@ -3,24 +3,25 @@ import os
 import sys
 import threading as thr
 
-IP = '127.0.0.1'
-PORT = 12000
+def get_ip_address(s : socket.socket):
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()
+
+# s.bind(get_ip_address(s))
+
 BUFFER_SIZE = 1024
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((IP, PORT))
-server_socket.listen(5)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+IP , PORT = get_ip_address(server_socket)
+#server_socket.listen(5)
 print(f'Server ready and listening on {IP}:{PORT}')
-clients = [] #List of clients, members should be tuples of IP and Ports
+clients = {} #List of clients, values should be tuples of IP and Ports
 try:
-    while True:  # section 4 step 6
-        (conn_socket, addr) = server_socket.accept()  # section 4 step 3
-        clients.append((conn_socket, addr))
-        #threader = thr.Thread(target=service_client_connection, args=(conn_socket,))
-        #threader.start()
+    while True:
+        pass
 except KeyboardInterrupt as ki:
     pass
-finally:
-    server_socket.close()
+#finally:
+#    server_socket.close()
 def receive():
     pass
 def send():# Probably gonna need multiple send and receive for each type
--- peer.py
+++ peer.py
@@ -1,3 +1,11 @@
 import socket
 import os
 import threading
+
+def get_ip_address(s : socket.socket):
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print(get_ip_address(s))
+# s.bind(get_ip_address(s))
+print("Binded")
