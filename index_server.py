91bf755fe97c522dc63e1aa1140a4872a151bfa1
Daniel Boyce <diboyce21students.desu.edu@Daniels-MacBook-Pro-2.local>
2024-05-01 13:26:00 -0400

    added threading to handle mutlpie peers

--- index_server.py
+++ index_server.py
@@ -1,11 +1,12 @@
 import socket
 import os
 import sys
+import threading
 import threading as thr
 import ip
 
 BUFFER_SIZE = 1024
-IP, PORT = "127.0.0.1", 12000
+IP, PORT = "127.0.0.1", 12001
 clients = {}
 
 
@@ -28,6 +29,29 @@ def connect_peer(peer_address):
     return a_socket
 
 
+def service_peer_connection(a_socket, address):
+    nickname = enter(a_socket)
+    clients.update({nickname: address})
+    print(f"{nickname} connected from {address}.")
+    a_socket.send(b"Welcome to the server!\n")
+    message_type = ""
+    message = a_socket.recv(BUFFER_SIZE)
+    message = message.decode().upper()
+    message_type = message_type
+    if message == 'S':
+        a_socket.send(b"Who would you like to chat with")
+        message = a_socket.recv(BUFFER_SIZE)
+        peer_ip, peer_port = clients.get(message)
+        connect_peer((peer_ip, peer_port))
+    elif message == 'G':
+        a_socket.send(str(clients).encode())
+    elif message == 'A':
+        pass
+    elif message == 'L':
+        remove(address)
+        a_socket.close()
+
+
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((IP, PORT))
@@ -38,28 +62,9 @@ def main():
         while True:
             peer_socket, address = server_socket.accept()
             print(f"Connection from {address} has been established.")
+            t1 = threading.Thread(target=service_peer_connection, args=(peer_socket, address,))
+            t1.start()
 
-            nickname = enter(peer_socket)
-            clients.update({nickname: address})
-            print(f"{nickname} connected from {address}.")
-            peer_socket.send(b"Welcome to the server!\n")
-            message_type = ""
-            while message_type.upper() != "L":
-                message = peer_socket.recv(BUFFER_SIZE)
-                message = message.decode().upper()
-                message_type = message_type
-                if message == 'S':
-                    peer_socket.send(b"Who would you like to chat with")
-                    message = peer_socket.recv(BUFFER_SIZE)
-                    peer_ip, peer_port = clients.get(message)
-                    connect_peer((peer_ip, peer_port))
-                elif message == 'G':
-                    peer_socket.send(str(clients).encode())
-                elif message == 'A':
-                    pass
-                elif message == 'L':
-                    remove(address)
-                    peer_socket.close()
     except KeyboardInterrupt:
         print("Server is shutting down.")
     finally:
--- ip.py
+++ ip.py
@@ -7,6 +7,6 @@ import threading as thr
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     #s.connect(("8.8.8.8", 80))
-    server_address = ("127.0.0.1", 12000)
+    server_address = ("127.0.0.1", 12001)
     s.connect(server_address)
     return s.getsockname() , server_address
