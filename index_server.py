6251fc9b7dac87a868a5e4a5794041ff6bb6b83b
Daniel Boyce <diboyce21students.desu.edu@Daniels-MacBook-Pro-2.local>
2024-04-30 22:53:24 -0400

    updates

--- index_server.py
+++ index_server.py
@@ -5,50 +5,53 @@ import threading as thr
 import ip
 
 BUFFER_SIZE = 1024
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# IP, PORT = ip.get_ip_address(server_socket)
-IP, PORT = "10.33.16.1", 12000
-IP = "127.0.0.1"
-server_socket.bind((IP, PORT))
-print(f'Server ready and listening on {IP}:{PORT}')
-clients = {}  # List of clients, values should be tuples of IP and Ports
+IP, PORT = "127.0.0.1", 12000
 
 
-def remove(address):
-    for x in clients.keys():
-        if clients.get(x) == address:
-            clients.pop(x)
-            return 1
-    print("Address not found")
+# def remove(address):
+#     for x in clients.keys():
+#         if clients.get(x) == address:
+#             clients.pop(x)
+#             return 1
+#     print("Address not found")
 
 
-def enter(a_socket):
-    nickname, address = a_socket.recvfrom(BUFFER_SIZE)
-    server_socket.sendto('Welcome to the server'.encode(), address)
-    print(nickname.decode(), "has entered the server at ", address)
-    return nickname, address
+def enter(peer_socket):
+    """Receive the client's nickname from the socket."""
+    nickname = peer_socket.recv(1024).decode('utf-8')
+    return nickname.strip()
 
 
-try:
-    nickname, address = enter(server_socket)
-    clients.update({nickname: address})
-    while True:
-        message, peer_address = server_socket.recvfrom(BUFFER_SIZE)
-        message_type = message.decode()[0].upper()
-        message = message.decode()[1:]
-        if message_type == 'S':
-            pass
-        elif message_type == 'G':
-            server_socket.sendto(str(clients).encode(), peer_address)
-            pass
-        elif message_type == 'A':
-            pass
-        elif message_type == 'L':
-            remove(address)
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((IP, PORT))
+    server_socket.listen()
+    print(f'Server ready and listening on {IP}:{PORT}')
 
-        pass
-except KeyboardInterrupt as ki:
-    pass
+    clients = {}
+
+    try:
+        while True:
+            peer_socket, address = server_socket.accept()
+            print(f"Connection from {address} has been established.")
+
+            nickname = enter(peer_socket)
+            clients.update({nickname: address})
+            print(f"{nickname} connected from {address}.")
+            peer_socket.send(b"Welcome to the server!\n")
+
+            # Here, add handling for additional data from clients if necessary
+    except KeyboardInterrupt:
+        print("Server is shutting down.")
+    finally:
+        server_socket.close()
+        for nickname, (sock, _) in clients.items():
+            sock.close()
+        print("Cleaned up all connections.")
+
+
+if __name__ == "__main__":
+    main()
 
 
 # finally:
